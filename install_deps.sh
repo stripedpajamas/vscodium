60735fe64c28c4f781bd6084aba9a6f04af132d3
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-10-11 20:41:44 -0400

    Added sources

--- install_deps.sh
+++ install_deps.sh
@@ -8,11 +8,13 @@ else
   sudo apt-get update
   sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm
   if [[ "$BUILDARCH" == "ia32" ]]; then
+    echo "Setting up env for i386"
     sudo dpkg --add-architecture i386
     sudo apt-get update
     sudo apt-get install libc6-dev-i386 gcc-multilib g++-multilib
     sudo apt-get install libx11-dev:i386 libxkbfile-dev:i386
   elif [[ $BUILDARCH == "arm64" ]]; then
+    echo "Setting up env for arm64"
     deps="libx11-dev libxkbfile-dev libsecret-1-dev"
     lib_path=/usr/lib/aarch64-linux-gnu
     linkage_list="-L $lib_path -I/usr/include/aarch64-linux-gnu"
@@ -21,7 +23,8 @@ else
     done
     export CC="aarch64-linux-gnu-gcc $linkage_list"
     export CXX="aarch64-linux-gnu-g++ $linkage_list"
-    dpkg --add-architecture arm64
+    sudo dpkg --add-architecture arm64
+    ./arm_sources.sh
     sudo apt-get update
     sudo apt-get install dpkg-cross
     sudo apt-get install libx11-dev:arm64 libxkbfile-dev:arm64 libsecret-1-dev:arm64
